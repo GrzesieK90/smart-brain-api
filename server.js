3887bde98ac5995bb7adc5895482622abcfc4284
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2022-12-19 21:08:10 +0100

    Adding database

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors())
 app.use(express.json()); 
 
-app.get('/', (req, res)=> { res.send('Everything is working') })
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
