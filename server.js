a107fc1a774e12e37b83cc18eeedd94941445441
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2022-12-21 12:51:07 +0100

    try do deploy with acces to database 1

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex');
-const { createClient } = require('@supabase/supabase-js')
 
 const register = require('./Controllers/register');
 const signin = require('./Controllers/signin');
 const profile = require('./Controllers/profile');
 const image = require('./Controllers/image');
 
-const db = createClient(
-  'https://mdaquwtehnfqgxfyqvvg.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kYXF1d3RlaG5mcWd4ZnlxdnZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE0NzUxMDIsImV4cCI6MTk4NzA1MTEwMn0.o-772Rj6SAFU7y4rPOUJlQAOrZE7zYVt5UcxR1pE8GA'
-);
+const db = {
+  DATABASE_URL: `postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}`,
+  PGDATABASE: 'railway',
+  PGHOST: 'containers-us-west-175.railway.app',
+  PGPASSWORD: 'oDTU5W1no0DM1pdY74tw',
+  PGPORT: 5728,
+  PGUSER: 'postgres'
+}
+
 
 const app = express();
 
