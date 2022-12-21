989a395aa0ef4218860bf87b955e31f1096040ce
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2022-12-21 18:18:23 +0100

    Back to local version

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
-const register = require('./Controllers/register');
-const signin = require('./Controllers/signin');
-const profile = require('./Controllers/profile');
-const image = require('./Controllers/image');
-const db = require('./prisma')
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'postgres',
+    password : 'tank',
+    database : 'smartbrain'
+  }
+});
 
 const app = express();
 
@@ -21,6 +31,4 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(3001, ()=> { console.log('app is running on port 3001') })
