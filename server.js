1ba360cb3233720fb7d2e5240f5fc0a01f1c6c3f
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2022-12-22 17:11:34 +0100

    Adding conection to database

--- server.js
+++ server.js
@@ -21,12 +21,11 @@ const app = express();
 app.use(cors())
 app.use(express.json()); 
 
-app.get('/', (req, res)=> { res.send('it is working') })
+app.get('/', (req, res)=> { res.json('it is working') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-const port = process.env.PORT 
-app.listen(port || 3000, ()=> { console.log(`App is running on port ${port}!`) })
+app.listen(process.env.PORT  || 3000, ()=> { console.log(`App is running on port ${process.env.PORT }!`) })
