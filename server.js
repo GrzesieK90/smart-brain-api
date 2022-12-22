23dbb383b6ec32776affd2effa85f0d01236a6f6
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2022-12-22 13:22:43 +0100

    reparing some details

--- server.js
+++ server.js
@@ -24,11 +24,12 @@ const app = express();
 app.use(cors())
 app.use(express.json()); 
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send('it is working') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3001, ()=> { console.log('App is running on port 3001!') })
+const port = process.env.PORT 
+app.listen(port || 3000, ()=> { console.log(`App is running on port ${port}!`) })
