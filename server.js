d4259d2ee0d250dc80ebbef09a4c31daa25b6ff2
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2022-12-22 16:50:12 +0100

    Adding conection to database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./Controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: 'process.env.DATABASE_URL',
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   }
 });
