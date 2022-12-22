2a5adf285a3a270bd18bfa9a3d5fc7b7486bc771
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2022-12-22 18:54:14 +0100

    update

--- Controllers/image.js
+++ Controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
  
 const app = new Clarifai.App({
- apiKey: '1fb946c122644ca697748ee5ba7c9a4b' 
+ apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
