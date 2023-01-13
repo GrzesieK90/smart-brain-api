8bd5a54cd016b5c06e171c6f2de23e60e8a8415c
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2023-01-13 20:38:56 +0100

    change to grpc server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
+// const cors = require('cors');
 const knex = require('knex');
 
 const register = require('./Controllers/register');
@@ -20,7 +20,7 @@ const db = knex({
 
 const app = express();
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json()); 
 
 app.get('/', (req, res)=> { res.send('it is working') })
