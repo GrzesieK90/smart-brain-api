403fae2a30d595e5acba295d846768c4e3bf6580
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2023-01-13 21:53:11 +0100

    change to grpc server

--- Controllers/image.js
+++ Controllers/image.js
@@ -3,7 +3,7 @@ const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
 const stub = ClarifaiStub.grpc();
 
 const metadata = new grpc.Metadata();
-metadata.set("authorization", "Key process.env.API_CLARIFAI");
+metadata.set("authorization", process.env.API_CLARIFAI);
  
 const handleApiCall = (req, res) => {
   stub.PostModelOutputs(
