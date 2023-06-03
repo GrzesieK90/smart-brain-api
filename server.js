ec4acb20829c3de09ff8c57fb90cda0d12acd28a
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2023-06-03 12:56:36 +0200

    new server

--- server.js
+++ server.js
@@ -10,13 +10,8 @@ const image = require('./Controllers/image');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host : 'containers-us-west-137.railway.app',
-    port : 6538,
-    user : 'postgres',
-    password : '16TyxHExLlnHYZXxVftD',
-    database : 'postgresql://postgres:16TyxHExLlnHYZXxVftD@containers-us-west-137.railway.app:6538/railway'
-  }
+  connection: process.env.DATABASE_URL,
+  searchPath: ['knex', 'public'],
 });
 
 const app = express();
@@ -31,5 +26,5 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(6538, ()=> { console.log('App is running on port 6538') })
+app.listen(process.env.PORT || 3000, ()=> { console.log(`App is running on port ${PORT}`) })
 
