c47c50ce75d7b3c95076114da89aa85f68934c32
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2023-06-03 12:45:56 +0200

    new server

--- server.js
+++ server.js
@@ -31,4 +31,4 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT  || 3000, ()=> { console.log(`App is running on port ${process.env.PORT }!`) })
+app.listen(6538, ()=> { console.log('App is running on port 5638') })
